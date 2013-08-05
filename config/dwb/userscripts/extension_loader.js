2a224ea06aaf9d01f2e734d52df837b9ef95154e
James Baumgarten <jebaum@ucla.edu>
2013-08-04 19:35:31 -0700

    mess with some dwb settings, new desktop background, restore tagbar settings

--- config/dwb/userscripts/extension_loader.js
+++ config/dwb/userscripts/extension_loader.js
@@ -1,33 +1,4 @@
 //!javascript
-//<adblock_subscriptions___SCRIPT
-extensions.load("adblock_subscriptions", {
-//<adblock_subscriptions___CONFIG
-
-// Shortcut to subscribe to a filterlist
-scSubscribe : null, 
-// Command to subscribe to a filterlist
-cmdSubscribe : "adblock_subscribe", 
-
-// Shortcut to unsubscribe from a filterlist
-scUnsubscribe : null, 
-
-// Command to unsubscribe from a filterlist
-cmdUnsubscribe : "adblock_unsubscribe",
-
-// Shortcut to update subscriptions and reload filter rules
-// Note that dwb will also update all subscriptions on startup
-scUpdate : null, 
-
-// Command to update subscriptions and reload filter rules
-// Note that dwb will also update all subscriptions on startup
-cmdUpdate : "adblock_update", 
-
-// Path to the filterlist directory, will be created if it doesn't exist. 
-filterListDir : data.configDir + "/adblock_lists"
-//>adblock_subscriptions___CONFIG
-
-});
-//>adblock_subscriptions___SCRIPT
 //<userscripts___SCRIPT
 extensions.load("userscripts", {
 //<userscripts___CONFIG
@@ -151,6 +122,7 @@ shortcutToggleAutoFocus : null,
 commandToggleAutoFocus : "ut_toggle_autofocus", 
 
 //>unique_tabs___CONFIG
+
 });
 //>unique_tabs___SCRIPT
 //<supergenpass___SCRIPT
@@ -201,3 +173,31 @@ stripSubdomains : true
 
 });
 //>supergenpass___SCRIPT
+//<adblock_subscriptions___SCRIPT
+extensions.load("adblock_subscriptions", {
+//<adblock_subscriptions___CONFIG
+
+// Shortcut to subscribe to a filterlist
+scSubscribe : null, 
+// Command to subscribe to a filterlist
+cmdSubscribe : "adblock_subscribe", 
+
+// Shortcut to unsubscribe from a filterlist
+scUnsubscribe : null, 
+
+// Command to unsubscribe from a filterlist
+cmdUnsubscribe : "adblock_unsubscribe",
+
+// Shortcut to update subscriptions and reload filter rules
+// Note that dwb will also update all subscriptions on startup
+scUpdate : null, 
+
+// Command to update subscriptions and reload filter rules
+// Note that dwb will also update all subscriptions on startup
+cmdUpdate : "adblock_update", 
+
+// Path to the filterlist directory, will be created if it doesn't exist. 
+filterListDir : data.configDir + "/adblock_lists"
+//>adblock_subscriptions___CONFIG
+});
+//>adblock_subscriptions___SCRIPT
