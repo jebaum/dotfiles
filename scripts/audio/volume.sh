a4fe34f71d76e2a3a7d1f0a4d46a90c26ee3c196
James Baumgarten <jebaum@ucla.edu>
2013-08-19 15:52:12 -0700

    i really overcomplicated volume keybinds. also clean up more vim stuff

--- scripts/audio/volume.sh
+++ scripts/audio/volume.sh
@@ -12,19 +12,21 @@ if [ $# != 2 ]; then
   exit
 fi
 
-if [ "$DEVICE" == "$XFI" ]; then # xfi has a gay scale. make it straight
-  OLDVOLUME=`amixer -M get $CONTROL | egrep -m 1 -o '[0-9]{1,3}\%{1}' | head -1 | sed s/%//`
-  VOLUME=$OLDVOLUME
-  if   [ $VOLUME == 0 -a $2 == "-" ]; then
-    exit
-  elif [ $VOLUME == 100 -a $2 == "+" ]; then
-    exit
-  fi
-  while [ $VOLUME == $OLDVOLUME ]; do
-    amixer set $CONTROL ${1}${2}
-    VOLUME=`amixer -M get $CONTROL | egrep -m 1 -o '[0-9]{1,3}\%{1}' | head -1 | sed s/%//`
-  done
-else
-  amixer set $CONTROL ${1}${2}
-fi
+amixer -M set $CONTROL ${1}${2}
+
+#if [ "$DEVICE" == "$XFI" ]; then # xfi has a gay scale. make it straight
+  #OLDVOLUME=`amixer -M get $CONTROL | egrep -m 1 -o '[0-9]{1,3}\%{1}' | head -1 | sed s/%//`
+  #VOLUME=$OLDVOLUME
+  #if   [ $VOLUME == 0 -a $2 == "-" ]; then
+    #exit
+  #elif [ $VOLUME == 100 -a $2 == "+" ]; then
+    #exit
+  #fi
+  #while [ $VOLUME == $OLDVOLUME ]; do
+    #amixer set $CONTROL ${1}${2}
+    #VOLUME=`amixer -M get $CONTROL | egrep -m 1 -o '[0-9]{1,3}\%{1}' | head -1 | sed s/%//`
+  #done
+#else
+  #amixer -M set $CONTROL ${1}${2}
+#fi
 
