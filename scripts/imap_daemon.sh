986660c764918694a0ae0695c5fb4fb60d9efacb
James Baumgarten <jebaum@ucla.edu>
2013-09-17 22:32:47 -0700

    make startup nicer

---
+++ scripts/imap_daemon.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+while true
+do
+  exec /usr/bin/offlineimap &> /tmp/offlineimap.log
+done
---
+++ scripts/startup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if ! pgrep mpd > /dev/null ; then
+  mpd
+  mpc pause
+fi
+
+if ! pgrep ncmpcpp > /dev/null ; then
+  urxvt -name ncmpcpp -e ncmpcpp &
+fi
+
+if ! pgrep mutt > /dev/null ; then
+  urxvt -name mutt    -e mutt &
+fi
+
+if ! pgrep ranger > /dev/null ; then
+  urxvt -name ranger  -e ranger &
+fi
+
+if [[ $(ps aux | grep "urxvt -name vps" | wc -l) != 2 ]]; then
+  urxvt -name vps -e zsh -c "mosh james@jbaumy.com" &
+fi
