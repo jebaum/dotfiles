db6b8f92e99d830c1879093f4f8798f0a6cbccb0
James Baumgarten <jebaum@ucla.edu>
2013-09-22 12:37:12 -0700

    use builtin pacman checkupdates instead of root crontab

---
+++ i3/conky/checkupdates.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+UPDATES=$(checkupdates | wc -l)
+
+if [[ ${UPDATES} == 0 ]]; then
+  echo -n No new packages
+elif [[ ${UPDATES} == 1 ]]; then
+  echo -n 1 new package
+else
+  echo -n ${UPDATES} new packages
+fi
+
+
