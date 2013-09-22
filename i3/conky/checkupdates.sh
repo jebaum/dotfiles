f5e9d8d004c100c96f1d5024ac47906d720a7378
James Baumgarten <jebaum@ucla.edu>
2013-09-22 12:55:56 -0700

    more tweaks, conky is dumb

--- i3/conky/checkupdates.sh
+++ i3/conky/checkupdates.sh
@@ -2,12 +2,12 @@
 
 UPDATES=$(checkupdates | wc -l)
 
-if [[ ${UPDATES} == 0 ]]; then
-  echo -n No new packages
-elif [[ ${UPDATES} == 1 ]]; then
-  echo -n 1 new package
+if [[ "${UPDATES}" == "0" ]]; then
+  echo -n "No new packages"
+elif [[ "${UPDATES}" == "1" ]]; then
+  echo -n "1 new package"
 else
-  echo -n ${UPDATES} new packages
+  echo -n "${UPDATES} new packages"
 fi
 
 
