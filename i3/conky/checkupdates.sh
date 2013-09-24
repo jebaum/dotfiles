430db9dde043f697490c70397dbefcd3c4cee3bd
James Baumgarten <jebaum@ucla.edu>
2013-09-24 12:02:03 -0700

    update submodules, remove 'files' subdomain from grabbing config, switch to gmail server for ucla in mutt, add gvim bugfix and nerdcomment settings to vimrc

--- i3/conky/checkupdates.sh
+++ i3/conky/checkupdates.sh
@@ -4,10 +4,15 @@ UPDATES=$(checkupdates | wc -l)
 
 if [[ "${UPDATES}" == "0" ]]; then
   echo -n "No new packages"
+  exit
 elif [[ "${UPDATES}" == "1" ]]; then
   echo -n "1 new package"
+  exit
 else
   echo -n "${UPDATES} new packages"
+  exit
 fi
+
+echo -n "No new packages"
 
 
