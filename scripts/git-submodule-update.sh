4d176358b1bad9983cabd82820f6df690ab473f3
James Baumgarten <jebaum@ucla.edu>
2013-11-01 04:26:11 -0700

    why did i do that

--- scripts/git-submodule-update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~/dotfiles
-git submodule foreach git checkout master
-git submodule foreach git pull
-git add vim/bundle/*
-git commit -m 'update submodules'
-cd ~/dotfiles/vim/bundle/vim-fugitive
-git checkout james
-git merge master
