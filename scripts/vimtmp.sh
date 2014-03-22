1036d11681d10c7168ca3b1de2e2a5fbe2ab8690
James Baumgarten <jebaum@ucla.edu>
2014-03-22 00:27:59 -0700

    move and refine vimtmp script

---
+++ scripts/vimtmp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TMPDIR='/tmp/vimtmp'
+mkdir -p ${TMPDIR}
+
+if [ "$1" == "last" ]; then
+    FILE="${TMPDIR}/$(ls -t /tmp/vimtmp | head -n 1)"
+else
+    TIMESTAMP="$(date +'%j_%H-%M-%S')"
+    FILE="${TMPDIR}/${TIMESTAMP}.txt"
+fi
+
+urxvt -name vimtmp -e vim "${FILE}"
+
+if [ -e "${FILE}" ]; then
+    NUMLINES=$(wc -l "${FILE}" | cut -d ' ' -f 1)
+    NUMCHARS=$(wc -m "${FILE}" | cut -d ' ' -f 1)
+    if [ "${NUMLINES}" -ne "1" ]; then
+        PLURAL='s'
+    fi
+    xclip "${FILE}"
+    # notify-send "Copied ${NUMLINES} line${PLURAL} and ${NUMCHARS} chars from ${FILE} into clipboard"
+fi
