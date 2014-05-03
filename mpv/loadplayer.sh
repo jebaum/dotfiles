3fb97d902f4aa5b9a906888bad4abd10b1fdd61f
James Baumgarten <jebaum@ucla.edu>
2014-05-03 14:48:24 -0700

    update ranger/mpv/i3 configs and scripts for nicer mp3 launching from ranger

---
+++ mpv/loadplayer.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+FIFO="/home/james/.mpv/mpvfifo"
+if [ ! -p "$FIFO" ]; then
+  mkfifo "$FIFO"
+fi
+
+TYPE="$1"
+shift
+
+# don't call this script anything with mpv in the name or pgrep sees it
+if pgrep mpv >/dev/null; then
+  for i in "$@"; do
+    echo loadfile \"$i\" 1 >"$FIFO"
+  done
+else
+  if [[ "$TYPE" = "video" ]]; then
+    mpv --force-window -input file="$FIFO" -- "$@" &>/dev/null &
+  elif [[ "$TYPE" == "audio" ]]; then
+    mpv --force-window -name rangermpvaudio -input file="$FIFO" -- "$@" &>/dev/null &
+  fi
+fi
+
