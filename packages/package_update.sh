986cd1b8644380ce828b26ca7d40c2bd8e4ad73d
James Baumgarten <jebaum@ucla.edu>
2014-10-06 00:29:31 -0700

    update package update script to use mkaurball

--- packages/package_update.sh
+++ packages/package_update.sh
@@ -3,7 +3,8 @@
 ### i3
 cd i3
 find . -maxdepth 1 -name 'i3*tar.?z' -delete
-makepkg --source
+makepkg
+mkaurball
 PACKAGE=$(find . -maxdepth 1 -name 'i3*tar.gz')
 gpg --quiet --decrypt ../aur_login.conf.gpg > temp
 aurploader -l temp --auto --remove-cookiejar "$PACKAGE"
