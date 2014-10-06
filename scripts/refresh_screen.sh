7f343cb6ffee18cefec7f3e82eef78b41eeef962
James Baumgarten <jebaum@ucla.edu>
2014-10-06 00:53:44 -0700

    epilepsy script to try to clear ghosting

---
+++ scripts/refresh_screen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# could also use lcdscrub screensaver in xscreensaver
+while true; do
+    xrefresh -solid green
+    xrefresh -solid red
+    xrefresh -solid blue
+    xrefresh -solid magenta
+    xrefresh -solid orange
+    xrefresh -solid black
+    xrefresh -solid yellow
+done
