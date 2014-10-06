7466eaa51b943c857aa58c0d389f66b2c507abe3
James Baumgarten <jebaum@ucla.edu>
2014-10-06 00:33:12 -0700

    update mpv launch from ranger

--- mpv/loadplayer.sh
+++ mpv/loadplayer.sh
@@ -18,6 +18,10 @@ else
   if [[ "$TYPE" = "video" ]]; then
     mpv --force-window -input file="$FIFO" -- "$@" &>/dev/null &
   elif [[ "$TYPE" == "audio" ]]; then
+    # at some point --force-window stopped working, then started again
+    # if it stops again, solution is to create a playlist file using mpv --playlist=
+    # newline separated, no need to escape spaces, stick it in a predefined location in /tmp
+    # then, start a new terminal with the mpv command to read out of the playlist file
     mpv --force-window --x11-name rangermpvaudio -input file="$FIFO" -- "$@" &>/dev/null &
   fi
 fi
