0a77dc848f6e97da34df67ecf7af8af7563c6eff
James Baumgarten <jebaum@ucla.edu>
2014-11-30 00:16:31 -0800

    GREP_OPTIONS env var is deprecated

--- zsh/lib/grep.zsh
+++ zsh/lib/grep.zsh
@@ -20,5 +20,6 @@ elif grep-flag-available --exclude=.cvs; then
 fi
 unfunction grep-flag-available
 
-export GREP_OPTIONS="$GREP_OPTIONS"
+# GREP_OPTIONS is deprecated
+# export GREP_OPTIONS="$GREP_OPTIONS"
 export GREP_COLOR='1;32'
