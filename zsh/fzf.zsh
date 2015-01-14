c16ec77e083fd3899c0d1cccd51ff5b402281505
James Baumgarten <jebaum@ucla.edu>
2015-01-13 22:56:05 -0800

    update more fzf stuff

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,17 +1,7 @@
-# Setup fzf function
-# ------------------
-unalias fzf 2> /dev/null
-fzf() {
-  /usr/bin/ruby /home/james/dotfiles/scripts/fzf/fzf "$@"
-}
-export -f fzf > /dev/null
-
-# Auto-completion
-# ---------------
-[[ $- =~ i ]] && source /home/james/dotfiles/scripts/fzf/fzf-completion.zsh
-
 # Key bindings
 # ------------
+export FZF_DEFAULT_OPTS="-x" # -x is a superset of the other modes
+                             # prepend string with single quote for exact match
 # CTRL-T - Paste the selected file path(s) into the command line
 __fsel() {
   set -o nonomatch
@@ -48,14 +38,18 @@ bindkey '^T' fzf-file-widget
 
 # ALT-D - cd into the selected directory
 fzf-cd-widget() {
-  # TODO maintain a cache for this? it's slow from ~
-  # different things for d and D?
-  # also make similar updates to the C-t thing, but ^t isn't a thing
-  # would also want a cache for ^T
-  cd "${$(set -o nonomatch; command find -L \
-    \( -path '*.wine-pipelight' -o -path '*.ivy2*' -o -path '*.texlive*' \
-    -o -path '*.git' -o -path '*.metadata' -o -path '*_notes' \) \
-    -prune -o -type d -print 2>/dev/null | fzf):-.}"
+
+   # TODO maintain a cache for this? it's slow from ~
+   # different things for d and D?
+   # also make similar updates to the C-t thing, but ^t isn't a thing
+   # would also want a cache for ^T
+   cd "${$(set -o nonomatch; command find -L \
+     \( -path '*.wine-pipelight' -o -path '*.ivy2*' -o -path '*.texlive*' \
+     -o -path '*.git' -o -path '*.metadata' -o -path '*_notes' \) \
+     -prune -o -type d -print 2>/dev/null | fzf):-.}"
+   # cd "${$(set -o nonomatch; command find -L * -path '*/\.*' -prune \
+           # -o -type d -print 2> /dev/null | fzf):-.}"
+
   zle reset-prompt
 }
 zle     -N    fzf-cd-widget
@@ -63,7 +57,7 @@ bindkey '^[d' fzf-cd-widget
 
 # CTRL-R - Paste the selected command from history into the command line
 fzf-history-widget() {
-  LBUFFER=$(fc -l 1 | fzf +m -n2..,.. | sed "s/ *[0-9*]* *//")
+  LBUFFER=$(fc -l 1 | fzf +s +m -n2..,.. | sed "s/ *[0-9*]* *//")
   zle redisplay
 }
 zle     -N   fzf-history-widget
