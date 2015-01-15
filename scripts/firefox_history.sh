bcb93e9f5cecc18a3ec52a11df0325b593f6374a
James Baumgarten <jebaum@ucla.edu>
2015-01-15 09:39:21 -0800

    mess with firefox history dmenu thing

--- scripts/firefox_history.sh
+++ scripts/firefox_history.sh
@@ -8,8 +8,14 @@ export PLACES_SQLITE="$HOME/.mozilla/firefox/7n3iaz0q.default/places.sqlite"
 
 # sqlite3 ~/.mozilla/firefox/n7i655jr.default/places.sqlite "SELECT printf('%s %s %s', datetime(last_visit_date/1000000, 'unixepoch', 'localtime'),title,url) FROM moz_places;"  
 
-firefox $(sqlite3 $PLACES_SQLITE "select printf('%s %s', moz_bookmarks.title, moz_places.url) from moz_bookmarks left outer join moz_places where moz_bookmarks.fk = moz_places.id and substr(moz_places.url, 0, 5) = 'http';" | dmenu -l 50 | rev | cut -d' ' -f1 | rev)
+THING=$(sqlite3 $PLACES_SQLITE "select printf('%s %s', moz_bookmarks.title, moz_places.url) from moz_bookmarks left outer join moz_places where moz_bookmarks.fk = moz_places.id and substr(moz_places.url, 0, 5) = 'http';" | dmenu -b -i -nf \#888888 -nb \#1D1F21 -sf \#ffffff -sb \#1D1F21 -fn -*-terminus-medium-*-*-*-14-*-*-*-*-*-*-* -l 40 | rev | cut -d' ' -f1 | rev)
 
+if [ -z "$THING" ]; then
+    echo "you didn't select anything"
+else
+    firefox $THING
+    ~/dotfiles/scripts/vimperator_hack.sh
+fi
 
 # convert datetime to normal person time - sort by this time?
 # separate fields with some useful character (maybe the default "|")
