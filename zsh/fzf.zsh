2406b572d57bdb00610cbd75f75a120c8d4deeb5
James Baumgarten <jebaum@ucla.edu>
2015-05-03 11:14:32 -0700

    update fzf shell functions and default options

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,14 +1,18 @@
 # Key bindings
 # ------------
-export FZF_DEFAULT_OPTS="-x" # -x is a superset of the other modes
-                             # prepend string with single quote for exact match
+export FZF_DEFAULT_OPTS="-x --inline-info" # -x is a superset of the other modes
+                                           # prepend string with single quote for exact match
+
+__fzfcmd() {
+  [ ${FZF_TMUX:-1} -eq 1 ] && echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
+}
+
 # CTRL-T - Paste the selected file path(s) into the command line
 __fsel() {
-  set -o nonomatch
-  command find * -path '*/\.*' -prune \
+  command find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
     -o -type f -print \
     -o -type d -print \
-    -o -type l -print 2> /dev/null | fzf -m | while read item; do
+    -o -type l -print 2> /dev/null | sed 1d | cut -b3- | $(__fzfcmd) -m | while read item; do
     printf '%q ' "$item"
   done
   echo
@@ -16,41 +20,34 @@ __fsel() {
 
 if [[ $- =~ i ]]; then
 
-if [ -n "$TMUX_PANE" -a ${FZF_TMUX:-1} -ne 0 -a ${LINES:-40} -gt 15 ]; then
-  fzf-file-widget() {
-    local height
-    height=${FZF_TMUX_HEIGHT:-40%}
-    if [[ $height =~ %$ ]]; then
-      height="-p ${height%\%}"
-    else
-      height="-l $height"
-    fi
-    tmux split-window $height "zsh -c 'source ~/.fzf.zsh; tmux send-keys -t $TMUX_PANE \"\$(__fsel)\"'"
-  }
-else
-  fzf-file-widget() {
-    LBUFFER="${LBUFFER}$(__fsel)"
-    zle redisplay
-  }
-fi
+fzf-file-widget() {
+  LBUFFER="${LBUFFER}$(__fsel)"
+  zle redisplay
+}
 zle     -N   fzf-file-widget
 bindkey '^T' fzf-file-widget
 
 # ALT-D - cd into the selected directory
 fzf-cd-widget() {
-
    # TODO when this gets ironed out, update the command in .config/ranger/commands.py too
    # maybe use an environment variable to keep them in sync?
    # TODO maintain a cache for this? it's slow from ~
-   # different things for d and D?
-   # also make similar updates to the C-t thing, but ^t isn't a thing
+   # different things for A-d and A-D?
+   # also make similar updates to the C-t thing, but ^t isn't a thing, ^t and ^T are indistinguishable
    # would also want a cache for ^T
    # alternatively, could do something like this:
    #    strings /var/lib/mlocate/mlocate.db | grep -E '^/' | fzf
    # read everything from the database, all directories will start with the '/' character
-   DIR="${$(find -L \( -path '*.wine-pipelight' -o -path '*.ivy2*' -o -path '*.texlive*' \
-     -o -path '*.git' -o -path '*.metadata' -o -path '*_notes' \) \
-     -prune -o -type d -print 2>/dev/null | fzf):-.}"
+
+
+  cd "${$(command find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+    -o -type d -print 2> /dev/null | sed 1d | cut -b3- | $(__fzfcmd) +m):-.}"
+
+   # DIR="${$(find -L \( -path '*.wine-pipelight' -o -path '*.ivy2*' -o -path '*.texlive*' \
+     # -o -path '*.git' -o -path '*.metadata' -o -path '*_notes' \) \
+     # -prune -o -type d -print 2>/dev/null | fzf):-.}"
+
+   # DIR="${$(strings /var/lib/mlocate/mlocate.db | grep "^${PWD}" 2>/dev/null | fzf):-.}"
    cd $DIR
    zle reset-prompt
 }
@@ -59,7 +56,19 @@ bindkey '^[d' fzf-cd-widget
 
 # CTRL-R - Paste the selected command from history into the command line
 fzf-history-widget() {
-  LBUFFER=$(fc -l 1 | fzf +s +m -n2..,.. | sed "s/ *[0-9*]* *//")
+  local selected restore_no_bang_hist
+  if selected=$(fc -l 1 | $(__fzfcmd) +s --tac +m -n2..,.. --tiebreak=index --toggle-sort=ctrl-r -q "$LBUFFER"); then
+    num=$(echo "$selected" | head -1 | awk '{print $1}' | sed 's/[^0-9]//g')
+    if [ -n "$num" ]; then
+      LBUFFER=!$num
+      if setopt | grep nobanghist > /dev/null; then
+        restore_no_bang_hist=1
+        unsetopt no_bang_hist
+      fi
+      zle expand-history
+      [ -n "$restore_no_bang_hist" ] && setopt no_bang_hist
+    fi
+  fi
   zle redisplay
 }
 zle     -N   fzf-history-widget
