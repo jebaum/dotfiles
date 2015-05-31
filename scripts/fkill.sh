ec3a3c050dc3bfa4b626dc1cff2d9347273ce505
James Baumgarten <jebaum@ucla.edu>
2015-05-30 20:08:46 -0700

    update fkill.sh for killing multiple processes

--- scripts/fkill.sh
+++ scripts/fkill.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 if [ "$1" == "all" ]; then
-  pid=$(ps -ef $UID | sed 1d | fzf -m | awk '{print $2}')
+  pid=($(ps -ef $UID | sed 1d | fzf -m | awk '{print $2}'))
   shift
 else
-  pid=$(ps -fu $UID | sed 1d | fzf -m | awk '{print $2}')
+  pid=($(ps -fu $UID | sed 1d | fzf -m | awk '{print $2}'))
 fi
 
 if [ "x$pid" != "x" ]; then
-  echo "killing:"
-  ps -ef $UID | awk "substr(\$2, 0) == $pid"
+  echo "PROCESSES TO BE KILLED"
+  echo "======================"
+  for p in ${pid[@]}; do
+    ps -ef $UID | awk "substr(\$2, 0) == $p"
+  done
+  echo "======================"
   kill -${1:-9} $pid
+  echo "done"
 fi
