837bc62b061f3979a79f940b87e5c26aef9b9a82
James Baumgarten <jebaum@ucla.edu>
2015-06-01 12:03:18 -0700

    dont overwrite arch package cache if something goes wrong

--- scripts/get_arch_packages.sh
+++ scripts/get_arch_packages.sh
@@ -3,12 +3,12 @@
 AURFILE="$HOME/.aur.dat"
 PACFILE="$HOME/.pacman.dat"
 
-# echo "Getting pacman packages..."
 # for i in /var/lib/pacman/sync/*.db; do
   # name=$(basename "$i")
   # name=${name%.*}
   # tar -ztf $i | cut -d/ -f1 | sort -u | sed "s/^/${name}\//"; done > $PACFILE
 
+echo "Getting pacman packages..."
 pacman -Ss | grep -E '^(community|core|extra|multilib)\/' | cut -d' ' -f 1 > $PACFILE
 
 echo "Getting aur packages..."
@@ -16,7 +16,12 @@ echo "Getting aur packages..."
 # perl -nle 'print $1 if /href='"'"'\/git\/aur-mirror.git\/tree\/([^'"'"']+)/' < $TMPFILE > $AURFILE
 
 # inspired by aurpkglist command provided by python3-aur package
-curl -# https://aur.archlinux.org/packages.gz | gunzip --stdout > $AURFILE
+IFS=' ' AURLIST=$(curl -# https://aur.archlinux.org/packages.gz | gunzip --stdout 2>/dev/null)
+if [ "$?" = "1" ]; then
+    echo "could not get aur package list. bad connection?"
+else
+    cat <<< $AURLIST > "$AURFILE"
+fi
 head -n 1 $AURFILE
 sed -i '1d' $AURFILE
 
