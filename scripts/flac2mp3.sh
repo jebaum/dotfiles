05f4ab8a2d5e0b33a187c01ead3a8ff3f2e988ac
James Baumgarten <jebaum@ucla.edu>
2016-10-18 16:26:34 -0700

    add alternative flac2mp3 conversion command

--- scripts/flac2mp3.sh
+++ scripts/flac2mp3.sh
@@ -5,3 +5,7 @@
 find "${@}" -print0 | xargs -0 -L 1 -P 4 bash -c 'flac -cd "${0}" | lame -V 0 - "${0/%flac/mp3}"'
 # -L 1 means take one line of input at a time
 # -P 4 means run up to 4 processes at a time
+# parallel-moreutils -i -j$(nproc) ffmpeg -i {} -qscale:a 0 {}.mp3 -- ./*.flac
+# mkdir flac
+# mv *.flac flac/
+# rename .flac.mp3 .mp3 *.mp3
