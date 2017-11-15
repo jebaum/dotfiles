c565de67df2429bfe6e054236bf42859784909c1
James Baumgarten <jebaum@ucla.edu>
2017-11-14 22:45:05 -0800

    improve rg commands and add prompts to zsh fzf functions

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,5 +1,6 @@
 # keep in sync with https://github.com/junegunn/fzf/blob/master/shell/key-bindings.zsh
 export FZF_DEFAULT_OPTS="--extended --bind='ctrl-space:toggle-preview,alt-a:select-all,alt-d:deselect-all' --inline-info --color fg:-1,bg:-1,hl:80,fg+:3,bg+:233,hl+:46,info:150,prompt:110,spinner:150,pointer:167,marker:174"
+export FZF_DEFAULT_COMMAND="rg --files -u 2>&1" # make sure errors are visible if there are any
 
 if [[ $- == *i* ]]; then # $- is shell flags, 'i' flag means interactive shell
 
@@ -9,12 +10,13 @@ __fzfcmd() { # currently does basically nothing, can be used to change behavior 
 
 # CTRL-T - Paste the selected file path(s) into the command line
 __fsel() {
+  # not using rg for this since I want directories listed as well, not just files
   local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
     -o -type f -print \
     -o -type d -print \
     -o -type l -print 2> /dev/null | cut -b3-"}"
   setopt localoptions pipefail 2> /dev/null
-  eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" $(__fzfcmd) -m "$@" | while read item; do
+  eval "$cmd" | FZF_DEFAULT_OPTS="--prompt='find . -mindepth 1 > ' --height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS" $(__fzfcmd) -m "$@" | while read item; do
     echo -n "${(q)item} "
   done
   local ret=$?
@@ -33,7 +35,7 @@ bindkey '^T' fzf-file-widget
 
 # ALT-T - Paste the selected entry from locate output into the command line
 fzf-locate-widget() {
-LBUFFER="${LBUFFER}$(locate / | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS" $(__fzfcmd) -m | while read item; do echo -n "${(q)item} "; done)"
+LBUFFER="${LBUFFER}$(locate / | FZF_DEFAULT_OPTS="--prompt='locate / > ' --height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS" $(__fzfcmd) -m | while read item; do echo -n "${(q)item} "; done)"
   local ret=$?
   zle redisplay
   typeset -f zle-line-init >/dev/null && zle zle-line-init
@@ -45,10 +47,10 @@ bindkey '\et' fzf-locate-widget
 
 # ALT-d and ALT-D - cd into the selected directory using find/locate
 fzf-cd-find-widget() {
-  local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-    -o -type d -print 2> /dev/null | cut -b3-"}"
+  local cmd="command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+    -o -type d -print 2> /dev/null | cut -b3-"
   setopt localoptions pipefail 2> /dev/null
-  local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m)"
+  local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--prompt='find . -mindepth 1 -type d > ' --height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS" $(__fzfcmd) +m)"
   if [[ -z "$dir" ]]; then
     zle redisplay
     return 0
@@ -64,8 +66,11 @@ fzf-cd-locate-widget() {
   # $PWD will be expanded inside sed, so use a nonprinting bullshit character
   # as sed delimiter to make sure the whatever $PWD is doesn't screw up the command
   # DIR="${$(strings /var/lib/mlocate/mlocate.db | grep "^/" 2>/dev/null | sed "s^${PWD}/" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse" $(__fzfcmd)):-.}"
-  DIR="${$(strings /var/lib/mlocate/mlocate.db | grep "^/" 2>/dev/null | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse" $(__fzfcmd)):-.}"
-  cd $DIR
+  # have to do strings command instead of using "locate" binary because locate doesn't offer option to only return directories
+  # but the way information is stored in the db file, only directories start with a `/' character
+  # not fantastic but it works. instead of using sudo, just make sure that mlocate.db is readable by my user
+  local dir="${$(strings /var/lib/mlocate/mlocate.db | grep "^/" 2>/dev/null | FZF_DEFAULT_OPTS="--prompt='strings mlocate.db | grep ^/ > ' --height ${FZF_TMUX_HEIGHT:-40%} --reverse" $(__fzfcmd)):-.}"
+  cd "$dir"
   zle reset-prompt
 }
 zle     -N    fzf-cd-locate-widget
@@ -79,7 +84,7 @@ fzf-history-widget() {
   local selected num
   setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
   selected=( $(fc -l 1 |
-    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS --tac -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(q)LBUFFER} +m" $(__fzfcmd)) )
+    FZF_DEFAULT_OPTS="--prompt='fc -l 1 > ' --height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS --tac -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort --query=${(q)LBUFFER} +m" $(__fzfcmd)) )
   local ret=$?
   if [ -n "$selected" ]; then
     num=$selected[1]
@@ -96,8 +101,7 @@ zle     -N   fzf-history-widget
 bindkey '^R' fzf-history-widget
 
 
-# CTRL-O and ALT-O - open file from ag/locate commands
-# TODO determine whether to use ag or rg based on rg availability
+# CTRL-O and ALT-O - open file from rg/locate commands
 fzf-edit-widget-rg() { fzf-edit-widget rg }
 fzf-edit-widget-rg-all() { fzf-edit-widget rg-all }
 fzf-edit-widget-locate() { fzf-edit-widget locate }
@@ -109,11 +113,13 @@ fzf-edit-widget() {
   local OLD_FZF_DEFALUT_OPTS=$FZF_DEFAULT_OPTS
   FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS --bind=ctrl-r:toggle-sort --query=${(q)LBUFFER} -m"
   if [ "$1" = "rg" ]; then
-    local filelist=( $(command rg -g '' --files 2>/dev/null | $(__fzfcmd)) )
-    # local filelist=( $(ag -g '.' 2>/dev/null | $(__fzfcmd)) )
+    FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --prompt='rg > '"
+    local filelist=( $(command rg --files 2>/dev/null | $(__fzfcmd)) )
   elif [ "$1" = "rg-all" ]; then
-    local filelist=( $(command rg -g '*' --files 2>/dev/null | $(__fzfcmd)) )
+    FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --prompt='rg -u > '"
+    local filelist=( $(command rg --files -u 2>/dev/null | $(__fzfcmd)) ) # includes hidden files
   elif [ "$1" = "locate" ]; then
+    FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --prompt='locate / > '"
     local filelist=( $(locate / 2>/dev/null | $(__fzfcmd)) )
   fi
   if [ -z "$filelist" ]; then
@@ -152,19 +158,22 @@ zle -N fzgshowkey fzgshowkey
 bindkey '\es' fzgshowkey
 
 
-# TODO determine whether to use ag or rg based on rg availability
 fzf-search-widget() {
     # rg '[^:]+:[0-9]+:.{5,}'   # matches lines with 5 characters or more after the metadata
     # rg '[^:]+:[0-9]+:$'       # matches empty lines
     # filelist=($(ag "." --nocolor 2>/dev/null | grep -Ev ':[0-9]+:$' | fzf -m | cut -f 1,2 -d ":" | tr '\n' ' '))
+    local OLD_FZF_DEFALUT_OPTS=$FZF_DEFAULT_OPTS
     if [ "$1" = "rg-all" ]; then
+        FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --prompt='rg -u ".{5,}" > '"
         local UARGS="-u"
     elif [ "$1" = "rg-all-hidden" ]; then
+        FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --prompt='rg -uu ".{5,}" > '"
         local UARGS="-uu"
     else
+        FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --prompt='rg ".{5,}" > '"
         local UARGS=""
     fi
-    filelist=($(command rg ".{7,}" $UARGS --color never --line-number 2>/dev/null | $(__fzfcmd) -m | cut -f 1,2 -d ":" | tr '\n' ' '))
+    filelist=($(command rg ".{5,}" $UARGS --color never --line-number 2>/dev/null | $(__fzfcmd) -m | cut -f 1,2 -d ":" | tr '\n' ' '))
     if [ -z "$filelist" ]; then
         zle redisplay
     else
@@ -178,6 +187,7 @@ fzf-search-widget() {
         BUFFER=$opencmd
         zle accept-line
     fi
+    FZF_DEFAULT_OPTS=$OLD_FZF_DEFALUT_OPTS
 }
 fzf-search-widget-rg() { fzf-search-widget rg }
 zle -N fzf-search-widget-rg
