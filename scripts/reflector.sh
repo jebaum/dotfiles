dae861cc8594240def561afd935b9987c4a9010f
James Baumgarten <jebaum@ucla.edu>
2019-03-17 12:20:42 -0700

    change reflector script to use rankmirrors

--- scripts/reflector.sh
+++ scripts/reflector.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# This will run reflector on mirrorlist, copying from backup first, overwriting
+
+# cp /etc/pacman.d/mirrorlist.backup /etc/pacman.d/mirrorlist &&
+# reflector --verbose --country 'United States' --latest 50 --protocol http --sort rate --save /etc/pacman.d/mirrorlist
+# curl -s 'https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on'| sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 -
 
 wget -O /etc/pacman.d/mirrorlist.backup https://www.archlinux.org/mirrorlist/all/ &&
-cp /etc/pacman.d/mirrorlist.backup /etc/pacman.d/mirrorlist &&
-reflector --verbose --country 'United States' --latest 50 --protocol http --sort rate --save /etc/pacman.d/mirrorlist
+rankmirrors -v -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+
