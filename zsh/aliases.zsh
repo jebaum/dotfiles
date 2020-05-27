88a377ef3c6e123b905351430e39adecab18c71e
jbbaumga <jbbaumga@amazon.com>
2020-05-26 23:23:36 -0700

    remove old/unused zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,4 @@
-alias aliases='vim ~/dotfiles/zsh/lib/aliases.zsh && source ~/dotfiles/zsh/lib/aliases.zsh'
 alias reload='source ~/dotfiles/zsh/aliases.zsh'
-alias lock='i3lock -b -c 000000'
 
 # command aliases
 alias sudo='sudo -E '
@@ -9,33 +7,24 @@ alias datednotes='nvim `date "+%Y-%m-%d"`.txt'
 alias fontlist='fc-list -f "%{family} : %{file}\n" :spacing=100 | sort'
 alias g='git'
 alias gsu='git submodule foreach git checkout master && git submodule foreach git pull'
-alias gvim='gvim -p'
 alias make='make -j4'
-alias mutt="rm -f $HOME/.mutt/mail/selected; mutt"
-alias neomutt="rm -f $HOME/.mutt/mail/selected; neomutt" # TODO handle this better
 alias myip='curl icanhazip.com'
 alias nvim='nvim -p'
 alias nvimlisten='rm -f /tmp/nvim; NVIM_LISTEN_ADDRESS=/tmp/nvim nvim -p'
 alias nvimapi='nvim --api-info | python2 -c "import msgpack, sys, yaml; print yaml.dump(msgpack.unpackb(sys.stdin.read()))" | less'
-alias nyan='nc -v nyancat.dakko.us 23' # nyan cat
 alias open='xdg-open'
-alias r='rg'
 alias rg='rg --colors "path:fg:green" --colors "path:style:bold" --colors "line:fg:magenta" --colors "match:bg:yellow" --colors "match:fg:black" --colors "match:style:nobold" --color=always '
 alias paclsorphans="pacman --query --deps --unrequired"
 alias pandoc='pandoc -V geometry:margin=0.5in -f markdown+hard_line_breaks'
 alias pandoczenburn='pandoc -V geometry:margin=0.5in -f markdown+hard_line_breaks --highlight-style=zenburn'
 alias pandocnohighlight='pandoc -V geometry:margin=0.5in -f markdown+hard_line_breaks --no-highlight'
 alias pandocunicode='pandoc --latex-engine=xelatex -V geometry:margin=0.5in -f markdown+hard_line_breaks' # requires texlive-latexextra on arch
-alias pong='ping 8.8.8.8 -c 4'
 alias pyhttp='python3 -m http.server 8080'
 alias redshift='redshift -l "50:8" -t "6500:5500" -b 1.0'
-alias tags='./.git/hooks/ctags >/dev/null'
 alias termcolors='~/dotfiles/scripts/color/colortest.pl -w -r'
-alias tree='tree -C'
 alias tron='ssh sshtron.zachlatta.com'
 alias vi='vim'
 alias vim='vim -p'
-alias vimtip='shuf -n 1 /home/james/Dropbox/Documents/Misc/learn/vim/vimtips.txt | cowsay -f $(ls /usr/share/cows | shuf -n 1)'
 alias xev="xev | grep -A2 --line-buffered '^KeyRelease' | sed -n '/keycode /s/^.*keycode \([0-9]*\).* (.*, \(.*\)).*$/\1 \2/p'"
 alias xprop='xprop | grep WM_CLASS && echo The first part of WM_CLASS is the instance, second part is the class'
 
@@ -69,59 +58,6 @@ pf() { # process filter
   fi
 }
 
-recordwindow() {
-    recordmydesktop --windowid $(xwininfo | awk '/Window id:/ {print $4}')
-}
-
-# manually create ~/.python, get python2 and python 3 virtualenv packages
-# useful to make syntastic use the correct python version, among other things
-venv() {
-    local activate=~/.python/$1/bin/activate
-    if [ -e "$activate" ] ; then
-        source "$activate"
-    else
-        echo "Error: Not found: $activate"
-    fi
-}
-venv2() { venv 2 }  # run `virtualenv2 2` in ~/.python
-venv3() { venv 3 }  # run `virtualenv 3` in ~/.python
-
-venv27() { venv 27 ; }
-function syn() {
-    local RIG="192.168.1.100"
-    local THINKPAD="192.168.1.101"
-
-    if [ "$HOST" = "rig6" ]; then
-        local CLIENT=$THINKPAD
-        local SERVER=$RIG
-    else
-        local CLIENT=$RIG
-        local SERVER=$THINKPAD
-    fi
-
-    nc -z $CLIENT 50000
-    STATUS=$?
-    if [ $STATUS -ne 0 ]; then
-        echo $CLIENT is unavailable
-        return 1
-    fi
-
-    ssh -p 50000 $CLIENT 'killall synergys; killall synergyc' &>/dev/null
-    killall synergys synergyc &>/dev/null
-
-    if [ "$1" = "kill" ]; then
-        return
-    fi
-
-    synergys &>/dev/null
-    ssh -p 50000 $CLIENT synergyc $SERVER &>/dev/null
-}
-
-alias transmission-remote='transmission-remote 9092'
-pget() {
-    torrentdaemonstart 2>/dev/null
-    pirate-get --port 9092 -t "$@"
-}
 torrentadd() {
     transmission-remote -a "${1-$(xsel)}"
 }
@@ -155,23 +91,6 @@ function up() {
   done
   cd $P
   export MPWD=$P
-}
-
-function quote()
-{
-  local WHO_COLOR="\e[0;33m"
-  local TEXT_COLOR="\e[0;35m"
-  local COLON_COLOR="\e[0;35m"
-  local END_COLOR="\e[m"
-
-  Q=$(curl -s --connect-timeout 2 "http://www.quotationspage.com/random.php3" | iconv -c -f ISO-8859-1 -t UTF-8 | grep -m 1 "dt ")
-  TXT=$(echo "$Q" | sed -e 's/<\/dt>.*//g' -e 's/.*html//g' -e 's/^[^a-zA-Z]*//' -e 's/<\/a..*$//g')
-  W=$(echo "$Q" | sed -e 's/.*\/quotes\///g' -e 's/<.*//g' -e 's/.*">//g')
-  if [ "$W" -a "$TXT" ]; then
-    echo "${WHO_COLOR}${W}${COLON_COLOR}: ${TEXT_COLOR}“${TXT}”${END_COLOR}"
-  else
-    quote
-  fi
 }
 
 user_commands=(
