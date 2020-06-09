b517269d7105166ff6be5d7c459e1dfc449d7879
James Baumgarten <jbbaumga@amazon.com>
2020-06-09 09:35:26 -0700

    misc updates

--- scripts/fzf_utilities.sh
+++ scripts/fzf_utilities.sh
@@ -138,6 +138,17 @@ fzgshow() { # git commit browser. view all at once with ctrl-o, or sequentially 
   done
 }
 
+fshow() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
+}
+
 # fzgsha - get git commit sha, copy to clipboard
 # example usage: git rebase -i `fzgsha`
 fzgsha() {
