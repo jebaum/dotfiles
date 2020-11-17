9744b1497c756a03c7d8b5bfea012f99957ba5a8
James Baumgarten <jebaum@ucla.edu>
2020-11-17 15:12:29 -0800

    add i3 bind for popup terminal and wrap xcwd startup in script

---
+++ i3/smart-terminal-start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+STARTUPDIR="$($HOME/dotfiles/i3/xcwd/xcwd)"
+
+# https://stackoverflow.com/questions/3685970/check-if-a-bash-array-contains-a-value
+# https://stackoverflow.com/questions/14366390/check-if-an-element-is-present-in-a-bash-array  <- alternative
+nonoDirs=("/run/user/1000" "/")
+if [[ " ${nonoDirs[@]} " =~ " ${STARTUPDIR} " ]]; then
+    STARTUPDIR="$HOME"
+fi
+
+STARTUPDIR="$STARTUPDIR" st
