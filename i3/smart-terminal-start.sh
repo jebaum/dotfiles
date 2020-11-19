1e98595022a1e07e74be08ef61baeb2f5da8c9c0
James Baumgarten <jebaum@ucla.edu>
2020-11-19 15:42:47 -0800

    add firenvim

--- i3/smart-terminal-start.sh
+++ i3/smart-terminal-start.sh
@@ -4,7 +4,7 @@ STARTUPDIR="$($HOME/dotfiles/i3/xcwd/xcwd)"
 
 # https://stackoverflow.com/questions/3685970/check-if-a-bash-array-contains-a-value
 # https://stackoverflow.com/questions/14366390/check-if-an-element-is-present-in-a-bash-array  <- alternative
-nonoDirs=("/run/user/1000" "/")
+nonoDirs=("/run/user/1000" "/run/user/1000/firenvim" "/")
 if [[ " ${nonoDirs[@]} " =~ " ${STARTUPDIR} " ]]; then
     STARTUPDIR="$HOME"
 fi
