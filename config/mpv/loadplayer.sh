04f96515eb16808bfc9780d652aa2a1b4df598f3
James Baumgarten <jebaum@ucla.edu>
2020-11-22 21:05:08 -0800

    update mpv option name

--- config/mpv/loadplayer.sh
+++ config/mpv/loadplayer.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# TODO make this (possibly optionally) use mpc for audio to enqueue a song in mpd instead of a background ranger instance?
-# maybe only if mpd is running, otherwise ranger to make sure it does actually play?
-FIFO="$HOME/.config/mpv/mpvfifo"
-if [ ! -p "$FIFO" ]; then
-  mkfifo "$FIFO"
-fi
-
+SOCKET=$HOME/.config/mpv/mpvsocket
 # TYPE argument is hardcoded in ranger's rifle.conf
 TYPE="$1"
 shift
@@ -14,17 +8,17 @@ shift
 # don't call this script anything with mpv in the name or pgrep sees it
 if pgrep mpv >/dev/null; then
   for i in "$@"; do
-    echo loadfile \"$i\" append >"$FIFO" # could also do 'append-play'
+    echo loadfile \"$i\" append | socat - "$SOCKET" # could also do 'append-play'
   done
 else
   if [[ "$TYPE" = "video" ]]; then
-    mpv --force-window -input-file="$FIFO" -- "$@" &>/dev/null &
+    mpv --force-window "$@" &>/dev/null &
   elif [[ "$TYPE" == "audio" ]]; then
     # at some point --force-window stopped working, then started again
     # if it stops again, solution is to create a playlist file using mpv --playlist=
     # newline separated, no need to escape spaces, stick it in a predefined location in /tmp
     # then, start a new terminal with the mpv command to read out of the playlist file
-    nohup mpv --osd-level=3 --osd-msg3='${filename}\n${time-pos} / ${duration}\n\n${playlist}' --osd-font-size=60 --force-window --x11-name="rangermpvaudio" -input-file="$FIFO" -- "$@" 2>&1 >/dev/null &
+    nohup mpv --osd-level=3 --osd-msg3='${filename}\n${time-pos} / ${duration}\n\n${playlist}' --osd-font-size=60 --force-window --x11-name="rangermpvaudio" -- "$@" 2>&1 >/dev/null &
   fi
 fi
 
