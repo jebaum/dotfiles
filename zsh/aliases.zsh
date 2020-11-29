5c83bc9420d08094349bd23b568dd439c62a4522
James Baumgarten <jebaum@ucla.edu>
2020-11-28 18:40:42 -0800

    update zsh config for bat and exa

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,14 +1,16 @@
 alias reload='source ~/dotfiles/zsh/aliases.zsh'
 
-# command aliases
+# useful for following log files:
+# tail -f /var/log/pacman.log | bat --paging=never -l log
+alias ls='exa --long --group-directories-first --group --header'
 alias sudo='sudo -E '
 alias sc='systemctl'
 alias beep='echo -en "\a"'
 alias datednotes='nvim `date "+%Y-%m-%d"`.txt'
 alias fontlist='fc-list -f "%{family} : %{file}\n" :spacing=100 | sort'
 alias g='git'
-alias gsu='git submodule foreach git checkout master && git submodule foreach git pull'
-alias make='make -j4'
+alias gsu='git submodule foreach git pull --rebase'
+alias make='make -j9'
 alias myip='curl icanhazip.com'
 alias nvim='nvim -p'
 alias nvimlisten='rm -f /tmp/nvim; NVIM_LISTEN_ADDRESS=/tmp/nvim nvim -p'
@@ -21,17 +23,11 @@ alias pandoczenburn='pandoc -V geometry:margin=0.5in -f markdown+hard_line_break
 alias pandocnohighlight='pandoc -V geometry:margin=0.5in -f markdown+hard_line_breaks --no-highlight'
 alias pandocunicode='pandoc --latex-engine=xelatex -V geometry:margin=0.5in -f markdown+hard_line_breaks' # requires texlive-latexextra on arch
 alias pyhttp='python3 -m http.server 8080'
-alias redshift='redshift -l "50:8" -t "6500:5500" -b 1.0'
 alias termcolors='~/dotfiles/scripts/color/colortest.pl -w -r'
 alias tron='ssh sshtron.zachlatta.com'
-alias vi='vim'
 alias vim='vim -p'
 alias xev="xev | grep -A2 --line-buffered '^KeyRelease' | sed -n '/keycode /s/^.*keycode \([0-9]*\).* (.*, \(.*\)).*$/\1 \2/p'"
 alias xprop='xprop | grep WM_CLASS && echo The first part of WM_CLASS is the instance, second part is the class'
-
-mem() {
-    ps -eo rss,pid,euser,args:100 --sort %mem | grep -v grep | grep -i $@ | awk '{printf $1/1024 "MB"; $1=""; print }'
-}
 
 pacrmorphans() {
     sudo pacman --remove --recursive $(pacman --quiet --query --deps --unrequired)
@@ -113,24 +109,4 @@ magnet_to_torrent() {
 zsh_stats() {
   fc -l 1 | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n20
 }
-
-# sample usage: `echo -e "${white}lol${NC}"`
-white='\e[1;37m'
-blue='\e[1;34m'
-green='\e[1;32m'
-red='\e[1;31m'
-purple='\e[0;35m'
-yellow='\e[1;33m'
-NC='\e[0m' # No Color
-
-# MORE COLOR CODES FOR ALIASING
-#Black       0;30     Dark Gray     1;30
-#Blue        0;34     Light Blue    1;34
-#Green       0;32     Light Green   1;32
-#Cyan        0;36     Light Cyan    1;36
-#Red         0;31     Light Red     1;31
-#Purple      0;35     Light Purple  1;35
-#Brown       0;33     Yellow        1;33
-#Light Gray  0;37     White         1;37
-
 # vim:filetype=sh
