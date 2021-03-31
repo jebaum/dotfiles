6da62cc2647dbfab72119a289218b3983c2635f7
James Baumgarten <jebaum@ucla.edu>
2021-03-30 17:24:16 -0700

    add fzf function to checkout PRs from github

--- scripts/fzf_utilities.sh
+++ scripts/fzf_utilities.sh
@@ -282,3 +282,37 @@ fzh() { # execute a thing from history
 fzhedit() { # put a thing from history on command line for editing. only works in zsh
   print -z $(([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
 }
+
+# https://seb.jambor.dev/posts/improving-shell-workflows-with-fzf/#pull-requests
+function fzprcheckout() {
+  local jq_template pr_number
+
+  jq_template='"'\
+'#\(.number) - \(.title)'\
+'\t'\
+'Author: \(.user.login)\n'\
+'Created: \(.created_at)\n'\
+'Updated: \(.updated_at)\n\n'\
+'\(.body)'\
+'"'
+
+  pr_number=$(
+    gh api 'repos/:owner/:repo/pulls' |
+    jq ".[] | $jq_template" |
+    sed -e 's/"\(.*\)"/\1/' -e 's/\\t/\t/' |
+    fzf \
+      --with-nth=1 \
+      --delimiter='\t' \
+      --preview='echo -e {2}' \
+      --preview-window=top:wrap |
+    sed 's/^#\([0-9]\+\).*/\1/'
+  )
+
+  if [ -n "$pr_number" ]; then
+    gh pr checkout "$pr_number"
+  fi
+}
+
+function fzprdiff() {
+  gh pr list | fzf --preview "gh pr diff --color=always {+1}"
+}
