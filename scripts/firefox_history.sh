6408321dd4ea335574daba30fbde715629413648
James Baumgarten <jebaum@ucla.edu>
2021-07-21 23:56:34 -0600

    fix firefox_history scripts and use fzf + floating st with it

--- scripts/firefox_history.sh
+++ scripts/firefox_history.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
 # TODO has issues with db being locked. may need to copy the file somewhere periodically and cache the results
-PLACES_SQLITE="$(find $HOME/.mozilla/firefox/ -maxdepth 1 -mindepth 1 -type d -iname '*.default' | xargs du -sh | sort -rh | head -1 | cut -f2)/places.sqlite"
-
-# URLS=$(sqlite3 ${PLACES_SQLITE} "SELECT url FROM moz_places;")
-# sqlite3 $PLACES_SQLITE "SELECT last_visit_date,title,url FROM moz_places;" | sed -e 's/|/   /g' > /tmp/ffh.txt
-
+PLACES_SQLITE="$(find $HOME/.mozilla/firefox/ -maxdepth 1 -mindepth 1 -type d -iname '*-release' | xargs du -sh | sort -rh | head -1 | cut -f2)/places.sqlite"
+REPLICA_PLACES_SQLITE="/tmp/places.sqlite"
+cp -f $PLACES_SQLITE $REPLICA_PLACES_SQLITE
 
 # sqlite3 ~/.mozilla/firefox/n7i655jr.default/places.sqlite "SELECT printf('%s %s %s', datetime(last_visit_date/1000000, 'unixepoch', 'localtime'),title,url) FROM moz_places;"  
 
-# THING=$(sqlite3 $PLACES_SQLITE "select moz_places.url moz_places where substr(moz_places.url, 0, 5) = 'http'" | dmenu -b -i -nf \#888888 -nb \#1D1F21 -sf \#ffffff -sb \#1D1F21 -fn -*-terminus-medium-*-*-*-14-*-*-*-*-*-*-* -l 40 | rev | cut -d' ' -f1 | rev)
-# 123 in cut comes from sqliterc, which has first column width of 120. 3 spaces are added on after that
-THING=$(sqlite3 -init "$HOME/dotfiles/scripts/sqliterc" $PLACES_SQLITE "select title,url from moz_places where substr(moz_places.url, 0, 5) = 'http'" 2>/dev/null | tac | dmenu -b -i -nf \#C0C0C0 -nb \#242024 -sf \#000000 -sb \#FFA000 -fn 'Deja Vu Sans Mono-10' -l 60 | cut -b 123-)
+# awk field separator comes from sqliterc, which prints that unicode thingy between the url and the title
+THING=$(sqlite3 -init "$HOME/dotfiles/scripts/sqliterc" $REPLICA_PLACES_SQLITE "select url,printf('%s', datetime(last_visit_date/1000000, 'unixepoch', 'localtime')),title from moz_places where substr(moz_places.url, 0, 5) = 'http'" 2>/dev/null | tac | fzf | awk -F' ━━━━➤ ' '{print $1}')
+
 if [ -z "$THING" ]; then
     echo "you didn't select anything"
 else
-    firefox $THING
-    # TODO actually focus a firefox window here using i3 instead of just workspace 2?
-    # TODO cache the result of running the above sqlite command somewhere, and cron it every couple minutes? with long history it takes a couple seconds which is annoying
-    ~/dotfiles/scripts/vimperator_hack.sh
+    firefox --new-tab "$THING"
 fi
-
-# convert datetime to normal person time - sort by this time?
-# separate fields with some useful character (maybe the default "|")
