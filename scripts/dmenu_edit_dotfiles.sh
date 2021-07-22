994e5f59ba494c8fee9bc8d5ae10571707932da8
James Baumgarten <jebaum@ucla.edu>
2021-07-21 23:57:01 -0600

    remove some old files from dmenu_edit_dotfiles script

--- scripts/dmenu_edit_dotfiles.sh
+++ scripts/dmenu_edit_dotfiles.sh
@@ -2,10 +2,6 @@
 
 myvar="\
 i3config                $HOME/dotfiles/i3/config
-rcnarrow1               $HOME/dotfiles/i3/conky/rcnarrow1
-rcnarrow2               $HOME/dotfiles/i3/conky/rcnarrow2
-rcnarrow3               $HOME/dotfiles/i3/conky/rcnarrow3
-rcwide                  $HOME/dotfiles/i3/conky/rcwide
 vimrc                   $HOME/dotfiles/vim/vimrc
 dmenu_edit_dotfiles     $HOME/dotfiles/scripts/dmenu_edit_dotfiles.sh
 firefox_history         $HOME/dotfiles/scripts/firefox_history.sh
@@ -20,7 +16,7 @@ tmux.conf               $HOME/dotfiles/tmux.conf"
 #bookmark_urls           $HOME/dotfiles/config/qutebrowser/bookmarks/urls
 
 
-config_file=$(echo "$myvar" | rofi -dmenu -i -width 100 -location 6 | sed -e "s/ \+ / /g" | cut -f 2 -d' ')
+config_file=$(echo "$myvar" | rofi -dmenu -i -width 100 -columns 2 -location 6 | sed -e "s/ \+ / /g" | cut -f 2 -d' ')
 
 echo $config_file
 if [ -z $config_file ]; then
